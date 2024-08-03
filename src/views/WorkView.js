a7991913cd4d4ba0769ffd12cff7df4f5da184ec
Chase <chasetherabbit@me.com>
2024-08-02 21:31:47 -0700

    Got Navbar close enough for now. Made additional views and a few styles.css files

---
+++ src/views/WorkView.js
@@ -0,0 +1,9 @@
+export default function WorkView() {
+    return (
+        <div className="workview-container">
+            <header>
+                Utica is an architecture firm based in Copenhagen, Denmark.
+            </header>
+        </div>
+    );
+}
