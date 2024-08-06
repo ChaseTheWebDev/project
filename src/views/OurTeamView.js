7df61e749d826394e8293987e3455a5e14485ea9
Chase <chasetherabbit@me.com>
2024-08-05 19:23:50 -0700

    Another 4 hours in. Had a major setback and had to re-work WorkView. Everything fixed now. Still a little off on pages, but not obsessing on it too much.

--- src/views/OurTeamView.js
+++ src/views/OurTeamView.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import '../styles/OurTeamView.css';
 
 export default function OurTeamView() {
     return (
-        <div>OurTeamView</div>
+        <div className='our-team-container'>
+            <div className='card-container'>
+                <div className='member-card'>
+                    <div className='member-image-1'></div>
+                    <h3 className='member-name'>First Name Last Name</h3>
+                    <p className='member-info'>
+                        It all begins with an idea. Maybe you want to launch a
+                        business. Maybe you want to turn a hobby into something
+                        more. Or maybe you have a creative project to share with
+                        the world. Whatever it is, the way you tell your story
+                        online can make all the difference.
+                    </p>
+                </div>
+                <div className='member-card'>
+                    <div className='member-image-2'></div>
+                    <h3 className='member-name'>First Name Last Name</h3>
+                    <p className='member-info'>
+                        It all begins with an idea. Maybe you want to launch a
+                        business. Maybe you want to turn a hobby into something
+                        more. Or maybe you have a creative project to share with
+                        the world. Whatever it is, the way you tell your story
+                        online can make all the difference.
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
--- src/views/PressView.js
+++ src/views/PressView.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import '../styles/PressView.css';
 
 export default function PressView() {
     return (
-        <div>PressView</div>
+        <div className='press-container'>
+            <div className='press-dual-container'>
+                <div className='press-box-1'>
+                    <h1>Press</h1>
+                </div>
+                <div className='press-box-2'>
+                    <blockquote>
+                        “Their buildings often establish expectations and then
+                        flip them, creating a sense of weightlessness and wonder.”
+                    </blockquote>
+                    <small>
+                        – Press Source
+                    </small>
+                    <blockquote>
+                        “Surprisingly, three of my favorite projects from last
+                        year came from the same small studio: Utica. With a 
+                        combination of deep research and spontaneous wit, their 
+                        practice has injected new ideas into an architectural 
+                        landscape that desperately needs them.”
+                    </blockquote>
+                    <small>-Press Source</small>
+                </div>
+            </div>
+        </div>
     );
 }
